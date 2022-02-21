aaec81ac66760eb6fc5ee297544f6d1b11d26ee9
Ojaswi2000 <ojaswiawasthi2025@gmail.com>
2022-02-21 23:34:12 +0530

    added final styles

--- src/App.js
+++ src/App.js
@@ -39,24 +39,24 @@ export default class App extends Component {
           </form>
 
           <div className="keypad">
-            <button onClick={this.handleClear} className="clear">Clear</button>            
-            <button onClick={this.handlebackspace} className="backspace">C</button>
-            <button name='/' onClick={this.handleClick}>&divide;</button>
+            <button onClick={this.handleClear} className="clear" id="high">Clear</button>            
+            <button onClick={this.handlebackspace} className="backspace" id="high">C</button>
+            <button name='/' onClick={this.handleClick} id="high">&divide;</button>
             <button name='7' onClick={this.handleClick}>7</button>
             <button name='8' onClick={this.handleClick}>8</button>
             <button name='9' onClick={this.handleClick}>9</button>
-            <button name='*' onClick={this.handleClick}>&times;</button>
+            <button name='*' onClick={this.handleClick} id="high">&times;</button>
             <button name='4' onClick={this.handleClick}>4</button>
             <button name='5' onClick={this.handleClick}>5</button>
             <button name='6' onClick={this.handleClick}>6</button>
-            <button name='-' onClick={this.handleClick}>&ndash;</button>
+            <button name='-' onClick={this.handleClick} id="high">&ndash;</button>
             <button name='1' onClick={this.handleClick}>1</button>
             <button name='2' onClick={this.handleClick}>2</button>
             <button name='3' onClick={this.handleClick}>3</button>
-            <button name='+' onClick={this.handleClick}>+</button>
+            <button name='+' onClick={this.handleClick} id="high">+</button>
             <button name='0' onClick={this.handleClick}>0</button>
             <button name='.' onClick={this.handleClick}>.</button>
-            <button name='=' onClick={this.handleEquals} className="equals">=</button>
+            <button name='=' onClick={this.handleEquals} className="equals" id="high">=</button>
           </div>
         </div>
       </>
