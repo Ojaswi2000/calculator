851cacbe22f6662901871212ba5ce822ba6a7a53
Ojaswi2000 <ojaswiawasthi2025@gmail.com>
2022-02-21 20:42:49 +0530

    added calculations

--- src/App.js
+++ src/App.js
@@ -4,6 +4,32 @@ export default class App extends Component {
   state={
     result:"",
   }
+
+  handleClick=(e)=>{
+    var {result}=this.state;
+    this.setState({
+      result:result.concat(e.target.name)
+    })
+  }
+
+  handleClear=()=>{
+    var {result}=this.state;
+    this.setState({
+      result:""
+    });
+  }
+
+  handlebackspace=()=>{
+    var {result}=this.state;
+    this.setState({
+      result:result.slice(0,-1)
+    });
+  }
+
+  handleEquals=()=>{
+    
+  }
+
   render() {
     return (
       <>
@@ -13,24 +39,24 @@ export default class App extends Component {
           </form>
 
           <div className="keypad">
-            <button>Clear</button>
-            <button>C</button>
-            <button>&divide;</button>
-            <button>7</button>
-            <button>8</button>
-            <button>9</button>
-            <button>&times;</button>
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
-            <button>&ndash;</button>
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button>+</button>
-            <button>0</button>
-            <button>.</button>
-            <button>=</button>
+            <button onClick={this.handleClear}>Clear</button>            
+            <button onClick={this.handlebackspace}>C</button>
+            <button name='/' onClick={this.handleClick}>&divide;</button>
+            <button name='7' onClick={this.handleClick}>7</button>
+            <button name='8' onClick={this.handleClick}>8</button>
+            <button name='9' onClick={this.handleClick}>9</button>
+            <button name='*' onClick={this.handleClick}>&times;</button>
+            <button name='4' onClick={this.handleClick}>4</button>
+            <button name='5' onClick={this.handleClick}>5</button>
+            <button name='6' onClick={this.handleClick}>6</button>
+            <button name='-' onClick={this.handleClick}>&ndash;</button>
+            <button name='1' onClick={this.handleClick}>1</button>
+            <button name='2' onClick={this.handleClick}>2</button>
+            <button name='3' onClick={this.handleClick}>3</button>
+            <button name='+' onClick={this.handleClick}>+</button>
+            <button name='0' onClick={this.handleClick}>0</button>
+            <button name='.' onClick={this.handleClick}>.</button>
+            <button name='=' onClick={this.handleEquals}>=</button>
           </div>
         </div>
       </>
